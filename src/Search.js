9dc2a0257a8d17d1effaaa2a380ac0cb5ccb022d
Corentin MAS <corentinmas@yahoo.com>
2021-10-29 18:25:28 +0200

    added search autocomplete

--- src/Search.js
+++ src/Search.js
@@ -2,12 +2,14 @@ import {useEffect, useState} from "react";
 import {useIsFocused} from "@react-navigation/native";
 import {Searchbar} from "react-native-paper";
 import * as React from "react";
+import {Text, View} from "react-native";
 
 export function Search({route, navigation}) {
     const {api, searchQuery} = route.params;
     const [data, setData] = useState();
     const isFocused = useIsFocused();
     const [newSeachQuery, setSearchQuery] = React.useState('');
+    const [autoCompleteVals, setAutoComplete] = React.useState([]);
 
 
     console.log('searchQuery', searchQuery)
@@ -15,11 +17,15 @@ export function Search({route, navigation}) {
     const sort = ['relevance', 'hot', 'top', 'new', 'comments'];
     const t = ['hour', 'day', 'week', 'month', 'year', 'all']
 
+    async function autoComplete(query) {
+        setSearchQuery(query)
+        api.makeRequest('https://oauth.reddit.com/api/subreddit_autocomplete_v2?limit=5&query=' + encodeURI(query)).then(res => {
+            setAutoComplete(res.data.children.map(v => v.data))
+        })
+    }
 
-
-    // todo: auto complete https://www.reddit.com/dev/api/#GET_api_subreddit_autocomplete_v2
-
-    const onChangeSearch = query => {
+    // todo sort by
+    const makeSearch = query => {
         // restrict_sr=false&limit=10&
         api.makeRequest('r/all/hot' /*+ encodeURIComponent(query)*/).then(data => {
             setData(data)
@@ -38,12 +44,18 @@ export function Search({route, navigation}) {
 
 
     return (
-        <Searchbar
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            defaultValue={searchQuery}
-            value={newSeachQuery}
-        />
+        <View>
+            <Searchbar
+                placeholder="Search"
+                onChangeText={autoComplete}
+                defaultValue={searchQuery}
+                value={newSeachQuery}
+            />
+
+            {autoCompleteVals.map(v => {
+                return (<Text>{v.display_name}</Text>) // todo mettre en forme
+            })}
+        </View>
     )
 
 }
